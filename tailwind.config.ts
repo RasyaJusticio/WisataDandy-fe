ca1f49f9eaafd972def56f0868d9aab6c25f5611
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-06-07 17:07:13 +0700

    feat: extend tailwind with website colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#26273b",
+        secondary: "#222338",
+        tertiary: "#1b1c31",
+        accent: {
+          "50": "#f0f3fd",
+          "100": "#e3e9fc",
+          "200": "#cdd5f8",
+          "300": "#aebaf3",
+          "400": "#8d96ec",
+          "500": "#7174e3",
+          "600": "#5d56d5",
+          "700": "#4f46bb",
+          "800": "#413c97",
+          "900": "#393679",
+          "950": "#222046",
+        },
       },
     },
   },
