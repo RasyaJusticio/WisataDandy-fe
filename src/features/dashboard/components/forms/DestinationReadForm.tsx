13018ff8c60e2e0865618ee558a1f3c78fcfa997
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-06-10 04:35:28 +0700

    feat(destination/read): style image display in the modal

--- src/features/dashboard/components/forms/DestinationReadForm.tsx
+++ src/features/dashboard/components/forms/DestinationReadForm.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import React from "react";
-import { Button, Dialog } from "react-aria-components";
+import {
+  Button,
+  Dialog,
+  DialogTrigger,
+  Heading,
+  Modal,
+  ModalOverlay,
+} from "react-aria-components";
 import { Row } from "../ui/Table";
 import { DestinationFormData } from "../..";
+import Image from "next/image";
+import Link from "next/link";
+import { PiX } from "react-icons/pi";
 
 type Props = Readonly<{
   dataSource: DestinationFormData | null | undefined;
@@ -45,18 +55,48 @@ const DestinationReadForm = ({ dataSource, close }: Props) => {
 
         <div className="input-group">
           <label htmlFor="imageInput">Gambar</label>
-          <a
-            href={`http://127.0.0.1:8000/storage/${dataSource?.image_url || ''}`}
-            type="button"
-          >
-            Lihat Gambar
-          </a>
+          <DialogTrigger>
+            <Button className="w-fit px-3 py-1.5 rounded-md transition-colors bg-accent-600 hover:bg-accent-700">
+              Lihat Gambar
+            </Button>
+            <ModalOverlay>
+              <Modal isDismissable>
+                <Dialog className="w-full md:w-fit m-auto p-3 bg-tertiary border-2 rounded-md border-border outline-none shadow-md pointer-events-auto">
+                  {({ close }) => (
+                    <div className="flex flex-col gap-2 items-center">
+                      <div className="flex w-full items-center">
+                        <Heading slot="title">Lihat Gambar</Heading>
+                        <Button className="ml-auto" onPress={close}>
+                          <PiX />
+                        </Button>
+                      </div>
+                      <div className="flex w-full md:w-96">
+                        <Image
+                          className="!relative !h-fit"
+                          src={`http://127.0.0.1:8000/storage/${
+                            dataSource?.image_url || ""
+                          }`}
+                          alt=""
+                          fill
+                        />
+                      </div>
+                    </div>
+                  )}
+                </Dialog>
+              </Modal>
+            </ModalOverlay>
+          </DialogTrigger>
         </div>
 
         <div className="actions">
-          <a href={`/destinasi/${dataSource?.slug}`} className="cancel">
+          <Link
+            target="_blank"
+            href={`/destinasi/${dataSource?.slug}`}
+            rel="noopener noreferer"
+            className="cancel"
+          >
             Lihat Halaman
-          </a>
+          </Link>
           <Button type="reset" className="cancel" onPress={close}>
             Kembali
           </Button>
