d70adf3ed5f2a9cca90666b472535934c6296c15
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-06-09 20:02:58 +0700

    feat(dashboard): setup CRUD operations for facilities

--- app/dashboard/fasilitas/page.tsx
+++ app/dashboard/fasilitas/page.tsx
@@ -1,26 +1,53 @@
 "use client";
 
 import {
+  DashboardModal,
   DashboardTable,
   facilityColumns,
   facilityService,
   TableRow,
 } from "@/src/features/dashboard";
-import React from "react";
+import React, { useState } from "react";
 
 const FacilityPage = () => {
   const { data, isLoading } = facilityService.useFacility();
 
+  const [isCreateModalOpen, setCreateModalOpen] = useState(false);
+  const [isUpdateModalOpen, setUpdateModalOpen] = useState(false);
+
+  const onCreate = () => {
+    setCreateModalOpen(true);
+  };
+
+  const onUpdate = () => {
+    setUpdateModalOpen(true);
+  };
+
+  const onDelete = () => {};
+
   return (
-    <div className="flex flex-col p-5">
-      <h2 className="text-xl font-semibold mb-5">Manajemen Fasilitas</h2>
-      <DashboardTable
-        columns={facilityColumns}
-        rows={data as TableRow[]}
-        dataName="Fasilitas"
-        loading={isLoading}
-      />
-    </div>
+    <>
+      <div className="flex flex-col p-5">
+        <h2 className="text-xl font-semibold mb-5">Manajemen Fasilitas</h2>
+        <DashboardTable
+          columns={facilityColumns}
+          rows={data as TableRow[]}
+          dataName="Fasilitas"
+          loading={isLoading}
+          onCreate={onCreate}
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+        />
+      </div>
+
+      <DashboardModal isOpen={isCreateModalOpen} setOpen={setCreateModalOpen}>
+        {({ close }) => <span>Create Modal</span>}
+      </DashboardModal>
+
+      <DashboardModal isOpen={isUpdateModalOpen} setOpen={setUpdateModalOpen}>
+        {({ close }) => <span>Update Modal</span>}
+      </DashboardModal>
+    </>
   );
 };
 
