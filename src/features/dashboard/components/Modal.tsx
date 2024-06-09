6553e8b9a8bbc7a19b633f9463a151f893d606e8
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-06-09 19:03:44 +0700

    feat(dashboard): style dashboard modal

--- src/features/dashboard/components/Modal.tsx
+++ src/features/dashboard/components/Modal.tsx
@@ -1,22 +1,26 @@
 "use client";
 
-import React from "react";
+import React, { ReactNode } from "react";
 import { Dialog, Modal } from "react-aria-components";
 
 type Props = Readonly<{
   isOpen: boolean;
   setOpen: (value: boolean) => void;
+  children?: (props: {
+    close: () => void;
+  }) => ReactNode | (() => ReactNode) | ReactNode;
 }>;
 
-const MyModal = ({ isOpen, setOpen }: Props) => {
+const MyModal = ({ isOpen, setOpen, children }: Props) => {
+  const close = () => {
+    setOpen(false);
+  };
+
   return (
-    <Modal
-      isDismissable
-      className="fixed top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.5)] grid place-items-center z-[100]"
-      isOpen={isOpen}
-      onOpenChange={setOpen}
-    >
-      <Dialog>asdfasdfa</Dialog>
+    <Modal isDismissable isOpen={isOpen} onOpenChange={setOpen}>
+      <Dialog className="w-full md:w-fit mt-auto md:m-auto p-3 bg-tertiary border-2 rounded-t-md md:rounded-md border-border outline-none shadow-md pointer-events-auto">
+        {typeof children === "function" ? children({ close }) : children}
+      </Dialog>
     </Modal>
   );
 };
